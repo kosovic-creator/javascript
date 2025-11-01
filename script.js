1c1128db07de2679122c0201298bb6ef5049dc63
kosovic-creator <drasko.kosovic@icloud.com>
2025-11-01 08:47:25 +0100

    int

--- script.js
+++ script.js
@@ -1,152 +1,147 @@
 const data = [
   {
     id: 1,
-    title: "The Lord of the Rings",
-    publicationDate: "1954-07-29",
-    author: "J. R. R. Tolkien",
-    genres: [
-      "fantasy",
-      "high-fantasy",
-      "adventure",
-      "fiction",
-      "novels",
-      "literature",
+        naslov: "The Lord of the Rings",
+        datumIzdanja: "1954-07-29",
+        autor: "J. R. R. Tolkien",
+        zanrovi: [
+            "fantastika",
+            "visoka fantastika",
+            "avantura",
+            "fikcija",
+            "romani",
+            "književnost",
     ],
-    hasMovieAdaptation: true,
-    pages: 1216,
-    translations: {
-      spanish: "El señor de los anillos",
-      chinese: "魔戒",
-      french: "Le Seigneur des anneaux",
-    },
-    reviews: {
-      goodreads: {
-        rating: 4.52,
-        ratingsCount: 630994,
-        reviewsCount: 13417,
-      },
-      librarything: {
-        rating: 4.53,
-        ratingsCount: 47166,
-        reviewsCount: 452,
-      },
-    },
+        filmPreveden: true,
+        stranica: 1216,
+        prevodi: {
+            španski: "El señor de los anillos",
+            kineski: "魔戒",
+            francuski: "Le Seigneur des anneaux",
+        }
   },
   {
     id: 2,
-    title: "The Cyberiad",
-    publicationDate: "1965-01-01",
-    author: "Stanislaw Lem",
-    genres: [
-      "science fiction",
-      "humor",
-      "speculative fiction",
-      "short stories",
-      "fantasy",
+      naslov: "The Cyberiad",
+      datumIzdanja: "1965-01-01",
+      autor: "Stanislaw Lem",
+      zanrovi: [
+          "fantastika",
+          "humor"
     ],
-    hasMovieAdaptation: false,
-    pages: 295,
-    translations: {},
-    reviews: {
-      goodreads: {
-        rating: 4.16,
-        ratingsCount: 11663,
-        reviewsCount: 812,
-      },
-      librarything: {
-        rating: 4.13,
-        ratingsCount: 2434,
-        reviewsCount: 0,
-      },
+      filmPreveden: false,
+      stranica: 295,
+      prevodi: {
+
     },
-  },
-  {
-    id: 3,
-    title: "Dune",
-    publicationDate: "1965-01-01",
-    author: "Frank Herbert",
-    genres: ["science fiction", "novel", "adventure"],
-    hasMovieAdaptation: true,
-    pages: 658,
-    translations: {
-      spanish: "",
-    },
-    reviews: {
-      goodreads: {
-        rating: 4.25,
-        ratingsCount: 1142893,
-        reviewsCount: 49701,
-      },
-    },
-  },
-  {
-    id: 4,
-    title: "Harry Potter and the Philosopher's Stone",
-    publicationDate: "1997-06-26",
-    author: "J. K. Rowling",
-    genres: ["fantasy", "adventure"],
-    hasMovieAdaptation: true,
-    pages: 223,
-    translations: {
-      spanish: "Harry Potter y la piedra filosofal",
-      korean: "해리 포터와 마법사의 돌",
-      bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
-      portuguese: "Harry Potter e a Pedra Filosofal",
-    },
-    reviews: {
-      goodreads: {
-        rating: 4.47,
-        ratingsCount: 8910059,
-        reviewsCount: 140625,
-      },
-      librarything: {
-        rating: 4.29,
-        ratingsCount: 120941,
-        reviewsCount: 1960,
-      },
-    },
-  },
-  {
-    id: 5,
-    title: "A Game of Thrones",
-    publicationDate: "1996-08-01",
-    author: "George R. R. Martin",
-    genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
-    hasMovieAdaptation: true,
-    pages: 835,
-    translations: {
-      korean: "왕좌의 게임",
-      polish: "Gra o tron",
-      portuguese: "A Guerra dos Tronos",
-      spanish: "Juego de tronos",
-    },
-    reviews: {
-      goodreads: {
-        rating: 4.44,
-        ratingsCount: 2295233,
-        reviewsCount: 59058,
-      },
-      librarything: {
-        rating: 4.36,
-        ratingsCount: 38358,
-        reviewsCount: 1095,
-      },
-    },
+
   },
 ];
 
-function getBooks() {
+function getKnjiga() {
   return data;
 }
 
-function getBook(id) {
+function getKnjigaById(id) {
   return data.find((d) => d.id === id);
 }
-const book = getBook(4);
-const author = book.author;
 
-console.log(author); // J. K. Rowling
+const knjige = getKnjiga();
+console.log(knjige);
 
-export { getBooks, getBook, author };
+const knjiga = getKnjigaById(2);
+console.log(knjiga);
+const { naslov, autor, zanrovi } = knjiga;
+console.log(naslov, autor, zanrovi);
+
+[prviZanr, drugiZanr, treciZanr, ...ostaliZanrovi] = zanrovi;
+console.log(prviZanr, drugiZanr, treciZanr, ostaliZanrovi);
+
+// Create a new object with all properties of 'knjiga' and add 'datumPublikacije'
+const knjigaSaDatumomPublikacije = { ...knjiga, datumPublikacije: '12-12-2020', stranica: 100 };
+console.log(knjigaSaDatumomPublikacije);
+
+const uslov = knjiga.stranica > 200 ? 'Duga knjiga' : 'Kratka knjiga';
+console.log(uslov);
+
+const uslov2 = knjiga.filmPreveden ? 'preveden film' : 'nije preveden film';
+console.log(uslov2);
+
+const uslov3 = knjiga.filmPreveden && knjiga.stranica < 300 ? 'preveden film' : 'Mala knjiga';
+console.log(uslov3);
+
+const uslov4 = !knjiga.filmPreveden;
+console.log(uslov4);
+
+const uslov5 = knjiga.filmPreveden || knjiga.stranica > 300;
+console.log(uslov5);
+
+const uslov6 = knjiga.godinaIzdanja < 2000 ? 'stara knjiga' : 'nova knjiga';
+console.log(uslov6);
+
+const uslov7 = knjiga.stranica > 300 && 'Duga knjiga';
+console.log(uslov7);
 
 
+function prikažiGodinuIzdanja() {
+    return knjiga.datumIzdanja.split('-')[0];
+}
+console.log(prikažiGodinuIzdanja());
+
+const prikažiGodinuIzdanja2 = () => knjiga.datumIzdanja.split('-')[0];
+console.log(prikažiGodinuIzdanja2());
+
+console.log(`Naslov knjige je: ${knjiga.naslov}, a autor je: ${knjiga.autor}.`);
+
+//filter, map, reduce
+//važno je knnjige biti niz
+const dugaKnjiga = knjige.filter(knjiga => knjiga.stranica > 300);
+console.log(dugaKnjiga);
+
+
+const nasloviKnjiga = knjige.map(knjiga => knjiga.naslov);
+console.log(nasloviKnjiga);
+
+const ukupnoStranica = knjige.reduce((ukupno, knjiga) => ukupno + knjiga.stranica, 0);
+console.log(ukupnoStranica);
+
+const sortiraneKnjige = knjige.sort((a, b) => a.stranica - b.stranica);
+console.log(sortiraneKnjige);
+const sortiraneKnjigeSlice = knjige.slice().sort((a, b) => a.stranica - b.stranica);
+console.log(sortiraneKnjigeSlice);
+
+//dodavanja nove knjige u niz
+const novaKnjiga = {
+    id: 3,
+    naslov: "Dune",
+    datumIzdanja: "1965-08-01",
+    autor: "Frank Herbert",
+    zanrovi: [
+        "fantastika",
+        "epična fantastika",
+        "naučna fantastika"
+    ],
+    filmPreveden: true,
+    stranica: 412,
+    prevodi: {
+        srpski: "Dina",
+        nemački: "Der Wüstenplanet",
+        italijanski: "Dune"
+    }
+};
+knjige.push(novaKnjiga);
+const azuriraniNizKnjiga = [...knjige, novaKnjiga];
+console.log(azuriraniNizKnjiga);
+
+const obrisiKnjiguById = (id) => {
+    // Remove the book with the given id from the knjige array
+    const index = knjige.findIndex((k) => k.id === id);
+    if (index !== -1) {
+        knjige.splice(index, 1);
+    }
+};
+obrisiKnjiguById(2);
+console.log(knjige);
+
+const azuriraniNizKnjigaNakonBrisanjaById = {}
+console.log(azuriraniNizKnjigaNakonBrisanjaById);
---
+++ script11.js
@@ -0,0 +1,9 @@
+const knjige = {
+    id: 2,
+    naslov: "The Cyberiad",
+    datumIzdanja: "1965-01-01",
+    autor: "Stanislaw Lem",
+    }
+
+const naslov = knjige.naslov;
+console.log(naslov);
