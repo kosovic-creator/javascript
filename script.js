12b2f93945e452d5a918f75894c82ada1f6fec38
kosovic-creator <drasko.kosovic@icloud.com>
2026-05-03 19:24:57 +0200

    init

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const data = [
-    {
-        id: 1,
-        name: "John Doe",
-        email: "john.doe@example.com"
-    }
-];
-console.log(data);
+// const broj = 10.9;
+//  x = broj.toString();
+// const x = broj.toFixed(2);
+// x=broj.toFixed(2);
+// x=broj.toPrecision(8);
+// x=broj.toFixed(0);
+// x=Math.round(broj);
+// x = Math.floor(broj);
+//const str = "heloo world      ";
+//x = str
+// x = str.toUpperCase();
+// str.trimStart(2);
+// str.trimEnd(6);
+//  x = str.trim();
+
+const x = 100 === "100";
+// const y = 100
+// x+=y;
+
+// x-=y;
+// x*=y;
+// x/=y;
+console.log(x);
