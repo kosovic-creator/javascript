691e7edd64cef1c4c4f20339f2a2c32b6193c180
kosovic-creator <drasko.kosovic@icloud.com>
2026-05-17 07:13:06 +0200

    ispravka

--- array(forEach,reduce,map..)/script.js
+++ array(forEach,reduce,map..)/script.js
@@ -3,6 +3,46 @@ const socialMedia = ["Facebook", "Twitter", "Instagram"];
 socialMedia.forEach((media) => {
   console.log(media);
 });
-// map
+// map pokazuje koliko znakova ima svaka društvena mreža
+
 const socialMediaLength = socialMedia.map((media) => media.length);
 console.log(socialMediaLength);
+const numbers = [1, 2, 3, 4, 5];
+// map - kvadrira svaki broj  pravi novi niz sa kvadratima brojeva iz originalnog niza
+const squaredNumbers = numbers.map((num) => num * num);
+console.log(squaredNumbers);
+// ili
+
+
+// reduce - zbroji dužinu svih naziva društvenih mreža
+const brojevi = [1, 2, 3, 4, 5];
+const total = brojevi.reduce((sum, num) => sum + num, 0);
+console.log(total);
+const totalLength = socialMedia.reduce((total, media) => total + media.length, 0);
+console.log(totalLength);
+
+// filter - filtrira društvene mreže koje imaju dužinu veću od 7 znakova
+const longNames = socialMedia.filter((media) => media.length > 7);
+console.log(longNames);
+
+const djeljivoSaDva = brojevi.filter((num) => num % 2 === 0);
+console.log(djeljivoSaDva);
+
+
+const companys = [
+  { name: "Google", employees: 100000 },
+  { name: "Facebook", employees: 50000 },
+    { name: "Instagram", employees: 20000 },
+];
+// filter - filtrira tvrtke koje imaju više od 30000 zaposlenika
+const bigCompanies = companys.filter((company) => company.employees > 30000);
+console.log(bigCompanies);
+
+const imeKompanije = companys.map((company) => {
+    return {
+        name: company.name,
+        employees: company.employees
+
+    };
+});
+console.log(imeKompanije);
--- petlje/script.js
+++ petlje/script.js
@@ -1,31 +1,87 @@
-const x ="drasko@gmail.com";
 
-if (x) {
-    console.log("This is truthy");
-} else {
-    console.log("This is falsy");
+for(let i = 0; i < 10; i++) {
+    console.log(i);
 }
 
-const djeca = ""; // Ovo je broj, ali nije validan broj
-
-if ((djeca!=undefined&&djeca!==""&&djeca!==null) ) {
-    console.log(`Imate ${djeca} djece`);
-} else {
-    console.log("Nemate djece");
+for(let i = 0; i < 10; i++) {
+    if(i === 5) {
+        break;
+    }
+    console.log(i);
 }
 
-const arr=[];
-if (arr.length>0) {
-    console.log("Niz nije prazan");
-} else {
-    console.log("Niz je prazan");
+//ukljanja 5 iz ispisa, ali nastavlja sa petljom
+for(let i = 0; i < 10; i++) {
+    if(i === 5) {
+        continue;
+    }
+    console.log(i);
 }
 
-const obj={
-    ime:"Drasko"
-};
-if (Object.keys(obj).length>0) {
-    console.log("Objekat nije prazan");
-} else {
-    console.log("Objekat je prazan");
+
+for(let i = 0; i < 10; i++) {
+    if(i === 5) {
+       console.log("Moj sretni broj"+i);
+    } else {
+        console.log("Broj je "+i);
+    }
 }
+
+
+for(let i = 0; i < 10; i++) {
+    console.log("Broj je "+i);
+
+    for(let j = 0; j < 10; j++) {
+
+            console.log(` ${i}*  ${j}= ${i*j}`);
+        }
+}
+
+let k1 = 0;
+while(k1 < 10) {
+    console.log(k1);
+    k1++;
+}
+
+let k = 10;
+do {
+    console.log(k);
+    k++;
+} while(k < 10);
+
+const items = ["apple", "banana", "orange"];
+const objectPerson =[ {
+    name: "John",
+    age: 30,
+    city: "New York"
+},
+{    name: "Jane",
+    age: 25,
+    city: "Los Angeles"
+}
+]
+for(let i = 0; i < items.length; i++) {
+    console.log(items[i]);
+}
+
+for(let item of items) {
+    console.log(item);
+}
+
+for (let item of items) {
+   console.log(item);
+    }
+for (let key in objectPerson) {
+    console.log(key + ": " + objectPerson[key].name);
+}
+
+
+
+
+
+
+
+
+
+
+
